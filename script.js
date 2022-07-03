b5fc2f071254415474b12df29df4797aa8e23434
baejeongwan <jayden.bae@outlook.kr>
2022-07-03 15:34:53 +0900

    Optimize for mobile & Fix about page

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const resultModal = new bootstrap.Modal(getId('resultModal'));
-const rulesModal = new bootstrap.Modal(getId('rulesModal'));
+
 const postViewModal = new bootstrap.Modal(getId('postViewModal'));
 const newsListModal = new bootstrap.Modal(getId('newsListModal'));
-let record;
 let newsList;
 
 init()
@@ -65,110 +63,13 @@ getId('resultModal').addEventListener('hide.bs.modal', event => {
     getId('examPage').classList.add('d-none');
 });
 
-function getStarted() {
-    getId('mainPage').classList.add('d-none');
-    getId('examPage').classList.remove('d-none');
-}
 
-function submitExam() {
-    let score = 0;
-    if (localStorage.length == 0) {
-        localStorage.setItem('tryOnDevice', 1);
-    } else {
-        let currentValue = parseInt(localStorage.getItem('tryOnDevice'));
-        localStorage.setItem('tryOnDevice', currentValue + 1);
-    }
-    record = {
-        try: localStorage.getItem('tryOnDevice'),
-        question1: false,
-        question2: false,
-        question3: false,
-        question4: false,
-        question5: 'none',
-        question6: getId('question-6').value,
-        totalScore: 0
-    };
-    if (getId('question-1').value >= 20) {
-        score += 20;
-        record.question1 = true;
-        console.log("1st question: ok")
-    }
-    if (getId('question-2-1').checked) {
-        score += 20;
-        record.question2 = true;
-        console.log("2nd question: ok")
-    }
-
-    if (getId('question-3-1').checked) {
-        score += 20;
-        record.question3 = true;
-        console.log("3rd question: ok")
-    }
-
-    if (getId('question-4-1').checked) {
-        score += 20;
-        record.question4 = true;
-        console.log("4th question: ok")
-    }
-
-    if (getId('question-5-1').checked || getId('question-5-2').checked) {
-        score += 20;
-        if (getId('question-5-1').checked) {
-            record.question5 = 'yes';
-        } else {
-            record.question5 = 'no';
-        }
-        console.log("5th question: ok")
-    }
-
-    //Clear
-    record.totalScore = score;
-    //Display result
-    if (score >= 70) {
-        //Pass
-        console.log('pass')
-        getId('resultText').classList.forEach(function (value, index, array) {
-            getId('resultText').classList.remove(value);
-        });
-        getId('resultText').classList.add('text-success');
-        getId('resultText').innerText = "통과했습니다! (" + score + " 점)";
-        getId('suggestText').innerText = "이 파일을 제출하십시오!";
-        getId('download').innerHTML = `
-        <button class="btn btn-primary btn-lg" onclick="downloadResult()">다운로드</button>`
-    } else {
-        //Fail
-        getId('resultText').classList.forEach(function (value, index, array) {
-            getId('resultText').classList.remove(value);
-        });
-        getId('resultText').classList.add('text-danger');
-        getId('resultText').innerText = "탈락했습니다! (" + score + " 점)";
-        getId('suggestText').innerText = "안녕히 가세요! (NaN점일 경우 모든 빈칸을 채웠는지 확인해주세요)";
-    }
-    resultModal.show();
-    //getId('examPage').reset();
-}
-
-function showRules() {
-    rulesModal.show();
-}
 
 //Function to shorten code
 function getId(id) {
     return document.getElementById(id);
 }
 
-function downloadResult() {
-    let downloadBlob = new Blob([JSON.stringify(record)], {type: 'application/json'})
-    let downloadTag = document.createElement('a');
-    downloadTag.href = URL.createObjectURL(downloadBlob);
-    downloadTag.download = "true";
-    downloadTag.click();
-    downloadTag.remove();
-}
-
-function clear() {
-    getId('examPage').reset();
-}
 
 async function loadFile() {
     let file = getId('fileBox').files[0];
